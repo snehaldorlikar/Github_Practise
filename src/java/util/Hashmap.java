57612beb25751340b16ad00d2668bf48cc02af56
shivkanya78kale <shivkanyakale78@gmail.com>
2023-03-31 16:57:17 +0530

    ShivkanyaCommit

--- src/java/util/Hashmap.java
+++ src/java/util/Hashmap.java
@@ -3,5 +3,11 @@ package java.util;
 public class Hashmap {
 public static void main(String[] args) {
 	HashMap<Integer,String> hp=new HashMap<>();
+	HashMap hm= new HashMap();
+	hm.put(1, "pune");
+	hm.put(2, "Mumbai");
+	hm.put(3, "Nagpur");
+	hm.put(4, "Jalna");
+	System.out.println(hm);
 }
 }
