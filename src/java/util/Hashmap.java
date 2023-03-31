6a0dde3045fcea9c755232f4b580b793e45fe01d
DipPaithane <deepapaithane98@gmail.com>
2023-03-31 16:44:16 +0530

    DeepaCommit

--- src/java/util/Hashmap.java
+++ src/java/util/Hashmap.java
@@ -3,5 +3,8 @@ package java.util;
 public class Hashmap {
 public static void main(String[] args) {
 	HashMap<Integer,String> hp=new HashMap<>();
+	hp.put(1, "Deepa");
+	hp.put(2, "snehal");
+	System.out.println(hp);
 }
 }
