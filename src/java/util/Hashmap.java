7eeeaecff5d82a643e241d07cb9cec9cab95c332
DipPaithane <deepapaithane98@gmail.com>
2023-03-31 17:31:28 +0530

    Change Request Done

--- src/java/util/Hashmap.java
+++ src/java/util/Hashmap.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	HashMap<Integer,String> hp=new HashMap<>();
 	hp.put(1, "Deepa");
 	hp.put(2, "snehal");
+	hp.put(3, "Mahesh");
 	System.out.println(hp);
 }
 }
