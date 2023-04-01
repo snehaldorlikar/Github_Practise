fb26f14fccbe40b4b977b77fad0d7b805b95a1f0
Prajkta <prajktawankhadepatil@gmail.com>
2023-04-01 16:57:46 +0530

    Conflict solved

--- src/java/util/Hashmap.java
+++ src/java/util/Hashmap.java
@@ -3,13 +3,13 @@ package java.util;
 public class Hashmap {
 public static void main(String[] args) {
 	HashMap<Integer,String> hp=new HashMap<>();
-<<<<<<< HEAD
-	hp.put(1, "Prajkta");
-=======
+
+	hp.put(4, "Prajkta");
+
 	hp.put(1, "Deepa");
 	hp.put(2, "snehal");
 	hp.put(3, "Mahesh");
->>>>>>> af8e64a5d68762f0d855956f3ffed4fb71b5c6a7
+
 	System.out.println(hp);
 }
 }
