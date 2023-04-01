629e29c3c0befeb765ad5a7f783ad32621dfc2b9
Mahesh <maheshchaudhari8998@gmail.com>
2023-04-01 16:23:20 +0530

    Mahesh Second commit

--- src/java/util/Hashmap.java
+++ src/java/util/Hashmap.java
@@ -6,6 +6,9 @@ public static void main(String[] args) {
 	hp.put(1, "Deepa");
 	hp.put(2, "snehal");
 	hp.put(3, "Mahesh");
+	hp.put(4, "Pankaj");
+	hp.put(5, "Aakash");
+	hp.put(6, "Avinash");
 	System.out.println(hp);
 }
 }
--- src/java/util/SonalClass.java
+++ src/java/util/SonalClass.java
@@ -2,7 +2,10 @@ package java.util;
 
 public class SonalClass {
 	public static void main(String[] args) {
+		
 		System.out.println("Sonal class - Java Program");
+		System.out.println("Ankita class - Java Program");
+		System.out.println("puja class - Java Program");
 	}
 
 }
